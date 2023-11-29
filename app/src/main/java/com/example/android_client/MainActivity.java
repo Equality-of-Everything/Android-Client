275755fa1613908f2622baaffab9617df710f331
xdxn <cc3523854674@163.com>
2023-11-29 11:22:11 +0800

    添加跳转
    登录页面跳转到注册

--- app/src/main/java/com/example/android_client/MainActivity.java
+++ app/src/main/java/com/example/android_client/MainActivity.java
@@ -2,17 +2,18 @@ package com.example.android_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtLoginUser, edtLoginPwd;
     private Button btnLogin;
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
@@ -31,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
 
 
     /**
@@ -56,6 +56,17 @@ public class MainActivity extends AppCompatActivity {
         edtLoginUser = findViewById(R.id.edt_login_user);
         edtLoginPwd = findViewById(R.id.edt_login_pwd);
         btnLogin = findViewById(R.id.btn_login);
+    }
+/**
+ * @param :
+ * @return void
+ * @author xcc
+ * @description 点击text跳转到注册页面
+ * @date 2023/11/29 11:13
+ */
+    public void jumpEnroll(View view) {
+        Intent intent = new Intent(this, EnrollActivity.class);
+        startActivity(intent);
     }
 }
 
