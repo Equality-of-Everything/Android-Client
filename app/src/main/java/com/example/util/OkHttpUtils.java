50418b2239982147aa76b6f0f619033e01d6ed10
AuroraZLee <I217650Shaw@163.com>
2023-11-29 14:13:32 +0800

    更新 OkHttpUtils.java

--- app/src/main/java/com/example/util/OkHttpUtils.java
+++ app/src/main/java/com/example/util/OkHttpUtils.java
@@ -18,6 +18,14 @@ public class OkHttpUtils {
 
     private static final OkHttpClient client = new OkHttpClient();
 
+    /**
+     * @param url:
+     * @param json:
+     * @return String
+     * @author Lee
+     * @description 封装OkHttp的网络请求功能
+     * @date 2023/11/29 14:08
+     */
     public static String sendJsonPostRequest(String url, String json) throws IOException {
         RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
         Request request = new Request.Builder()
