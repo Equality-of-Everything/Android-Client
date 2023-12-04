715986bd8831312b283e03f81782e66eda630087
xdxn <cc3523854674@163.com>
2023-12-04 21:34:38 +0800

    实现播放视频

--- app/src/main/java/com/example/UI/map/Map_VideoActivity.java
+++ app/src/main/java/com/example/UI/map/Map_VideoActivity.java
@@ -1,16 +1,47 @@
 package com.example.UI.map;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
 import com.example.android_client.R;
 import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
 
 public class Map_VideoActivity extends AppCompatActivity {
 
+    private PlayerView playerView;
+    private ExoPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_video);
+        //将播放器附加到视图
+        playerView = findViewById(R.id.map_video);
+        initializePlayer();
+    }
+
+
+    private void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+        Uri videoUri = Uri.parse("https://video.699pic.com/videos/05/12/19/a_qWPD4CwqVDej1600051219_10s.mp4");
+        MediaItem mediaItem = MediaItem.fromUri(videoUri);
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.play();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
     }
 }
