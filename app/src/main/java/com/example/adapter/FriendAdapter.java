761b26bba3b37ec632c115d44a4d52bd8748fc5a
AuroraZLee <I217650Shaw@163.com>
2023-12-12 10:42:47 +0800

    新加了好友列表
    下拉可以加载更多好友

---
+++ app/src/main/java/com/example/adapter/FriendAdapter.java
@@ -0,0 +1,62 @@
+package com.example.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android_client.R;
+import com.hyphenate.chat.EMContact;
+
+import java.util.List;
+
+/**
+ * @Author : Lee
+ * @Date : Created in 2023/12/11 21:51
+ * @Decription : 好友列表的Adapter
+ */
+
+public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.FriendViewHolder>{
+
+    private List<EMContact> friendList;
+
+    public FriendAdapter(List<EMContact> friendList) {
+        this.friendList = friendList;
+    }
+
+    @NonNull
+    @Override
+    public FriendAdapter.FriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.friend_item_layout, parent, false);
+        return new FriendViewHolder(view);
+    }
+
+    // 绑定数据到ViewHolder
+    @Override
+    public void onBindViewHolder(@NonNull FriendAdapter.FriendViewHolder holder, int position) {
+        EMContact friend = friendList.get(position);
+        holder.tvFriendName.setText(friend.getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return friendList.size();
+    }
+
+    // ViewHolder类
+    public static class FriendViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivFriendAvatar;
+        TextView tvFriendName;
+
+        public FriendViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivFriendAvatar = itemView.findViewById(R.id.iv_friend_avatar);
+            tvFriendName = itemView.findViewById(R.id.tv_friend_name);
+        }
+    }
+
+}
