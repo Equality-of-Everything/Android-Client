131fdce6e98bc15e872556352d7a050cceb49369
Gibran <3470380398@qq.com>
2023-12-13 15:51:37 +0800

    1、完成退出登录

--- app/src/main/java/com/example/UI/mine/MineFragment.java
+++ app/src/main/java/com/example/UI/mine/MineFragment.java
@@ -14,11 +14,13 @@ import androidx.fragment.app.Fragment;
 
 import com.example.android_client.LoginActivity;
 import com.example.android_client.R;
+import com.example.util.TokenManager;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 
 public class MineFragment extends Fragment {
     private Button btnFriends;
+    private Button btnLogout;
 
     @Nullable
     @Override
@@ -26,9 +28,32 @@ public class MineFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_mine, container, false);
 
         btnFriends = view.findViewById(R.id.btn_friends);
+        btnLogout = view.findViewById(R.id.btn_logout);
+
+        setListenerForLogout();
+
         setListeners();
 
         return view;
+    }
+
+    /**
+     * @param :
+     * @return void
+     * @author zhang
+     * @description 为登出按钮绑定点击事件
+     * @date 2023/12/13 15:41
+     */
+    private void setListenerForLogout() {
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TokenManager.deleteExpiredTokenFromSharedPreferences(getActivity());
+                //点击跳转登出页面
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void setListeners() {
