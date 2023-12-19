43bf5b12fe102d540e9ce54ae6bb3c415d1758f1
AuroraZLee <I217650Shaw@163.com>
2023-12-19 21:55:04 +0800

    更新了聊天界面的头像，明天优化

--- app/src/main/java/com/example/adapter/ChatAdapter.java
+++ app/src/main/java/com/example/adapter/ChatAdapter.java
@@ -1,6 +1,9 @@
 package com.example.adapter;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,15 +11,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.android_client.R;
+import com.hyphenate.EMValueCallBack;
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMTextMessageBody;
+import com.hyphenate.chat.EMUserInfo;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * @Author : Lee
@@ -47,8 +55,39 @@ public class ChatAdapter extends ArrayAdapter<EMMessage> {
             view = inflater.inflate(R.layout.item_chat_receive, parent, false); // 接收方消息的布局
         }
 
-        ImageView avatar = view.findViewById(R.id.msg_avatar);
-        loadingAvatarImage()
+        ImageView messageAvatar = view.findViewById(R.id.msg_avatar);
+        // 使用好友的 ID 来获取头像属性
+        String[] userId = new String[1];
+        userId[0] = message.conversationId();
+        EMClient.getInstance().userInfoManager().fetchUserInfoByUserId(userId, new EMValueCallBack<Map<String, EMUserInfo>>() {
+            @Override
+            public void onSuccess(Map<String, EMUserInfo> value) {
+                // 获取用户头像属性成功后，在主线程中加载用户头像
+                EMUserInfo userInfo = value.get(userId[0]);
+                if (userInfo != null) {
+                    String avatarUrl = userInfo.getAvatarUrl();
+                    Log.e("FriendAdapter", "获取用户头像成功：" + "avatarUrl" + avatarUrl);
+                    // 在主线程中加载头像图片
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            // 加载头像图片
+                            Glide.with(getContext())
+                                    .load(avatarUrl)
+                                    .placeholder(R.drawable.loading)
+                                    .error(R.drawable.friend_item)
+                                    .into(messageAvatar);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onError(int error, String errorMsg) {
+                // 处理加载头像失败的情况
+                Log.e("FriendAdapter", "获取用户头像失败：" + error + ", " + errorMsg);
+            }
+        });
 
         // 设置消息内容
         TextView messageText = view.findViewById(R.id.msg_text);
