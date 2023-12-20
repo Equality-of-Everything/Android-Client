b5fbc4352a31e48e6cf49774ca0c7df42fd57039
tcy <1972228514@qq.com>
2023-12-20 09:57:26 +0800

    tcy更新个人资料编辑页面，新增好友列表item个性签名展示

--- app/src/main/java/com/example/UI/mine/PersonDataEditActivity.java
+++ app/src/main/java/com/example/UI/mine/PersonDataEditActivity.java
@@ -2,16 +2,57 @@ package com.example.UI.mine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.android_client.R;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 public class PersonDataEditActivity extends AppCompatActivity {
+    private Button mPickDateButton;
+    private TextView mShowSelectedDateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_mine);
+
+        //出生日期选择
+        mPickDateButton = findViewById(R.id.pick_date_button);
+        mShowSelectedDateText = findViewById(R.id.show_selected_date);
+        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
+        materialDateBuilder.setTitleText("选择日期");
+        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+        mPickDateButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        materialDatePicker.show(getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+                    }
+                });
+
+        // now handle the positive button click from the
+        // material design date picker
+        materialDatePicker.addOnPositiveButtonClickListener(
+                new MaterialPickerOnPositiveButtonClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+
+                        // if the user clicks on the positive
+                        // button that is ok button update the
+                        // selected date
+                        mShowSelectedDateText.setText("出生日期: " + materialDatePicker.getHeaderText());
+                        // in the above statement, getHeaderText
+                        // is the selected date preview from the
+                        // dialog
+                    }
+                });
+
     }
 
 }
