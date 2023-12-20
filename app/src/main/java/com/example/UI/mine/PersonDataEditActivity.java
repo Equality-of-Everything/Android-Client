8d8ae36363ed56ebcfd22c92853b0c16b01e3754
xdxn <cc3523854674@163.com>
2023-12-20 12:07:10 +0800

    更改个人资料，信息还没有传到服务器

--- app/src/main/java/com/example/UI/mine/PersonDataEditActivity.java
+++ app/src/main/java/com/example/UI/mine/PersonDataEditActivity.java
@@ -3,19 +3,38 @@ package com.example.UI.mine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android_client.R;
+import com.example.entity.UserLogin;
+import com.example.util.TokenManager;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PersonDataEditActivity extends AppCompatActivity {
     private Button mPickDateButton;
     private TextView mShowSelectedDateText;
-
+    private TextInputEditText mSignatureEditText ;
+    private TextInputEditText mEmailEditText ;
+    private Button mSaveButton;
+    private RadioGroup mradioGroup;
+    private RadioButton radio_button_1;
+    private RadioButton radio_button_2;
+    private RadioButton radio_button_3;
+    private static final String TAG = "PersonDataActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +43,14 @@ public class PersonDataEditActivity extends AppCompatActivity {
         //出生日期选择
         mPickDateButton = findViewById(R.id.pick_date_button);
         mShowSelectedDateText = findViewById(R.id.show_selected_date);
+        mradioGroup = findViewById(R.id.radioGroup);
+        mSignatureEditText = findViewById(R.id.signature_edit);
+        mEmailEditText = findViewById(R.id.email_edit);
+        mSaveButton = findViewById(R.id.confirm_change);
+        radio_button_1 = findViewById(R.id.radio_button_1);
+        radio_button_2 = findViewById(R.id.radio_button_2);
+        radio_button_3 = findViewById(R.id.radio_button_3);
+
         MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
         materialDateBuilder.setTitleText("选择日期");
         final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
@@ -34,6 +61,22 @@ public class PersonDataEditActivity extends AppCompatActivity {
                         materialDatePicker.show(getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
                     }
                 });
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 获取个性签名
+                String signature = mSignatureEditText.getText().toString();
+                // 获取邮箱地址
+                String email = mEmailEditText.getText().toString();
+                // 获取性别
+                int selectedRadioButtonId = mradioGroup.getCheckedRadioButtonId();
+                RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
+                String gender = selectedRadioButton.getText().toString();
+                // 获取出生日期
+                String birthDate  = mShowSelectedDateText.getText().toString();
+                saveUserDataAndSendToServer(signature, email, gender, birthDate);
+            }
+        });
 
         // now handle the positive button click from the
         // material design date picker
@@ -46,13 +89,68 @@ public class PersonDataEditActivity extends AppCompatActivity {
                         // if the user clicks on the positive
                         // button that is ok button update the
                         // selected date
-                        mShowSelectedDateText.setText("出生日期: " + materialDatePicker.getHeaderText());
+                        mShowSelectedDateText.setText("出生日期: " +materialDatePicker.getHeaderText());
                         // in the above statement, getHeaderText
                         // is the selected date preview from the
                         // dialog
                     }
                 });
+        loadUserData();
+    }
 
+    private void saveUserDataAndSendToServer(String signature, String email, String gender, String birthDate) {
+        // 检查信息是否为空
+        if (signature.isEmpty() || email.isEmpty() || gender.isEmpty() || birthDate.isEmpty()) {
+            Toast.makeText(this, "请填写完整信息", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (!isValidEmail(email)) {
+            // 电子邮件地址无效，显示错误信息给用户
+            Toast.makeText(this, "电子邮件无效", Toast.LENGTH_SHORT).show();
+            return;
+        }else {
+            // 保存用户个人信息到SharedPreferences
+            saveUserData(signature, email, gender, birthDate);
+            Toast.makeText(PersonDataEditActivity.this, "修改成功", Toast.LENGTH_SHORT).show();
+            //将信息上传到服务器
+        }
+    }
+
+    private boolean isValidEmail(String email) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // 保存用户个人信息
+    private void saveUserData(String signature, String email, String gender, String birthDate) {
+        SharedPreferences sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("signature", signature);
+        editor.putString("email", email);
+        editor.putString("gender", gender);
+        editor.putString("birthDate", birthDate);
+        editor.apply();
+    }
+    // 加载用户个人信息
+    private void loadUserData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        String signature = sharedPreferences.getString("signature", "");
+        String email = sharedPreferences.getString("email", "");
+        String gender = sharedPreferences.getString("gender", "");
+        String birthDate = sharedPreferences.getString("birthDate", "");
+        // 将加载的个人信息设置到界面上
+        mSignatureEditText.setText(signature);
+        mEmailEditText.setText(email);
+        // 设置性别的选择状态
+        if (gender.equals("男")) {
+            radio_button_1.setChecked(true);
+        } else if (gender.equals("女")) {
+            radio_button_2.setChecked(true);
+        } else {
+            radio_button_3.setChecked(true);
+        }
+        mShowSelectedDateText.setText(birthDate);
     }
 
 }
